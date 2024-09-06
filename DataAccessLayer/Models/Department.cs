87c96266a3814a6a867066e23d115b04062d8fbf
mohamedFathy98 <mdx7700@gmail.com>
2024-09-06 18:48:32 +0300

    Vid 02

---
+++ DataAccessLayer/Models/Department.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Models
+{
+    internal class Department
+    {
+        public int Id { get; set; } //pk
+
+        public int Code { get; set; }
+
+        public string Name { get; set; }
+        public DateTime DateOfCreation { get; set; }
+    }
+}
