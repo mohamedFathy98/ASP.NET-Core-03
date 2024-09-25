4c25e60d231873300fbb5ba10057fbd79e0d03bf
mohamedFathy98 <mdx7700@gmail.com>
2024-09-26 01:10:37 +0300

    vid => 04 - 05

---
+++ ASP.NET Core 03/ViewModels/RoleViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ASP.NET_Core_03.ViewModels
+{
+    public class RoleViewModel
+    {
+        public string? Id { get; set; }
+        public string? Name { get; set; }
+    }
+}
---
+++ ASP.NET Core 03/ViewModels/UserInRoleviewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ASP.NET_Core_03.ViewModels
+{
+    public class UserInRoleviewModel
+    {
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public bool IsInRole { get; set; }
+    }
+}
